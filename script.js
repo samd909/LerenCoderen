48bf3c84c168cdef5729bd932ea7cc79c4abc775
SefvandenHeurik <Sef.van.den.Heurik@student.gildeopleidingen.nl>
2024-09-04 10:32:25 +0200

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('javascriptbutton').addEventListener('click',()=> {
+    window.location.href = 'Test.html'
+})  
+
+document.getElementById('laravelbutton').addEventListener('click',()=> {
+    window.location.href = 'Test.html'
+})  
