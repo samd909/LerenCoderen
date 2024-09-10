7e21119af155976d0a554e5e7c66c79a719d2544
SefvandenHeurik <Sef.van.den.Heurik@student.gildeopleidingen.nl>
2024-09-10 13:30:42 +0200

    .

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.getElementById('javascriptbutton').addEventListener('click',()=> {
-    window.location.href = 'Test.html'
+    window.location.href = 'javascript.html'
 })  
 
 document.getElementById('laravelbutton').addEventListener('click',()=> {
-    window.location.href = 'Test.html'
+    window.location.href = 'laravel.html'
 })  
+
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
