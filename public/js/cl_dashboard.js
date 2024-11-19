6596679fec1594248a3db28976488a0f976b1cb6
Tristán <Tristan.Nouwens@student.gildeopleidingen.nl>
2024-11-19 14:08:44 +0100

    V2
    t

--- public/js/cl_dashboard.js
+++ public/js/cl_dashboard.js
@@ -11,11 +11,27 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+handleNav("home");
 function handleNav(target) {
   if (target === "logout") {
     if (confirm("Want to leave this page?")) {
       localStorage.removeItem("user");
       window.location.href = baseUrl;
+    }
+  } else {
+    const sections = document.querySelectorAll(".main-content > section");
+    sections.forEach((section) => {
+      section.classList.remove("active");
+      section.style.display = "none";
+    });
+
+    const activeSection = document.getElementById(target);
+    if (activeSection) {
+      activeSection.style.display = "block";
+      activeSection.classList.add("active");
+      activeSection.style.display = "flex";
+    } else {
+      console.error(`Section with id "${target}" does not exist.`);
     }
   }
 }
